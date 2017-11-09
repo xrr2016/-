9551aa137ed30efa5ae18bbf8e494988ab63eeeb
xrr2016 <xiaoranran1993@163.com>
2017-11-09 18:12:02 +0800

    :sparkles: working

--- src/myTab.js
+++ src/myTab.js
@@ -1,12 +1,13 @@
 class MyTab {
-  constructor(config) {
-    const opts = Object.assign(MyTab.opts, config)
+  constructor(config = {}) {
+    let opts = Object.create(MyTab.opts)
+    opts = Object.assign(opts, config)
     if (!opts.selector) throw new Error('需要一个放置Tab的容器。')
     this.selector = opts.selector
     this.currentIndex = opts.currentIndex
     this.defaultIndex = opts.defaultIndex
     this.theme = opts.theme
-    this.flexWidth = opts.flexWidth
+    this.fixedWidth = opts.fixedWidth
     this.color = opts.color
     this.tabHeight = opts.tabHeight
     this.timingFunc = opts.timingFunc
@@ -15,6 +16,7 @@ class MyTab {
     this.triggerEvent = opts.triggerEvent
     this.loop = opts.loop
     this.container = document.querySelector(this.selector)
+    this.insertStyle()
     this.init()
   }
 
@@ -37,7 +39,7 @@ class MyTab {
     this.indexContentItem()
   }
 
-  node2Array (nodeList) {
+  node2Array(nodeList) {
     return Array.prototype.slice.call(nodeList)
   }
 
@@ -52,43 +54,145 @@ class MyTab {
   generateTabs() {
     const tabList = document.createElement('ul')
     tabList.classList.add('my-tab-list')
-    if (this.theme === 'card') {
-      tabList.classList.add('card')
-    }
     this.tabs.forEach((tab, index) => {
       const li = document.createElement('li')
       li.classList.add('tab')
       li.innerHTML = tab.title
       li.dataset.index = index
       li.style.color = this.color
-      if (this.theme === 'card') {
-        li.classList.add('card')
-      }
-      if (this.flexWidth) {
-        li.style.flex = 1
+      if (this.fixedWidth) {
+        li.classList.add('fixed-width')
       }
       if (tab.disabled) {
         li.classList.add('disabled')
       }
       tabList.appendChild(li)
     })
+    if (this.theme === 'card') {
+      tabList.classList.add('card')
+      tabList.firstChild.classList.add('card')
+    }
+    if (this.fixedWidth) {
+      tabList.classList.add('fixed-width')
+    }
     return tabList
   }
 
   generateIndicator() {
-    const width = Math.floor(this.tabList.firstChild.getBoundingClientRect().width)
+    const width = Math.floor(
+      this.tabList.firstChild.getBoundingClientRect().width
+    )
     const indicator = document.createElement('div')
     indicator.classList.add('indicator')
+    if (this.theme === 'card') {
+      indicator.classList.add('card')
+    }
     indicator.style.width = width + 'px'
     indicator.style.background = this.color
     return indicator
   }
 
-  insertStyle() {}
+  insertStyle() {
+    const styleEle = document.createElement('style')
+    styleEle.innerHTML = `.my-tab {
+      width: 90%;
+      max-width: 1000px;
+      margin: 10px auto;
+      overflow: hidden;
+      background: #ffffff;
+      box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12),
+        0 3px 1px -2px rgba(0, 0, 0, 0.2);
+    }
+    .my-tab.card {
+      border: 1px solid #e2e2e2;
+      box-shadow: 0 2px 5px 0 rgba(0,0,0,.1);
+    }
+    .my-tab .my-tab-list {
+      position: relative;
+      margin: 0;
+      padding: 0;
+      list-style: none;
+      border-bottom: 1px solid #e2e2e2;
+    }
+    .my-tab .my-tab-list.card {
+      background: #f2f2f2;
+    }
+    .my-tab .my-tab-list.fixed-width {
+      display: flex;
+      justify-content: space-around;
+      align-items: center;
+    }
+    
+    .my-tab .my-tab-list .indicator {
+      position: absolute;
+      left: 0;
+      bottom: -1px;
+      height: 2px;
+      background: blueviolet;
+      will-change: left, right, transform;
+      transition: all .4s ease-in-out;
+      transform: scaleX(1);
+    }
+    .my-tab .my-tab-list .indicator.card {
+      opacity: 0;
+    }
+    .my-tab .my-tab-list .tab {
+      position: relative;
+      display: inline-block;
+      min-width: 65px;
+      padding: 0 24px;
+      margin: 0 -1px;
+      height: 40px;
+      line-height: 40px;
+      color: blueviolet;
+      font-weight: 500;
+      text-align: center;
+      cursor: pointer;
+      border: 1px solid transparent;
+      vertical-align: baseline;
+    }
+    .my-tab .my-tab-list .tab.fixed-width {
+      flex: 1;
+    }
+    .my-tab .my-tab-list .tab.card {
+      border-left: 1px solid #e2e2e2;
+      border-right: 1px solid #e2e2e2;
+      background: #ffffff;
+    }
+    .my-tab .my-tab-list .tab.card::after {
+      position: absolute;
+      left: 0;
+      bottom: -2px;
+      content: '';
+      width: 100%;
+      height: 1px;
+      z-index: 100;
+      background: #ffffff;
+    }
+    .my-tab .my-tab-list .tab.disabled {
+      cursor: default;
+    }
+    .my-tab .my-tab-content {
+      position: relative;
+      display: flex;
+      justify-content: flex-start;
+      align-content: center;
+    }
+    
+    .my-tab .my-tab-content .item {
+      width: 100%;
+      max-width: 100%;
+      height: 100%;
+      padding: 12px 24px;
+      transform: translateX(0px);
+      transition: all .4s ease-in-out;
+    }
+    `
+    document.head.appendChild(styleEle)
+  }
 
   handleTabChange(event) {
     const target = event.target
-    console.log(target)
     if (target.classList.contains('disabled')) return
     const index = parseInt(target.dataset.index, 10)
     const targetW = target.getBoundingClientRect().width
@@ -125,7 +229,7 @@ MyTab.opts = {
   currentIndex: 0,
   defaultIndex: 0,
   theme: 'flat', // card
-  flexWidth: false,
+  fixedWidth: false,
   color: 'blueviolet',
   width: 600,
   tabHeight: 40,
@@ -140,8 +244,25 @@ MyTab.opts = {
   moveTime: 300
 }
 
-new MyTab({
+const tab1 = new MyTab({
+  selector: '.my-tab-1'
+})
+
+const tab2 = new MyTab({
   selector: '.my-tab-2',
   theme: 'card',
-  color: 'goldenrod'
+  color: '#3498db'
+})
+
+const tab3 = new MyTab({
+  selector: '.my-tab-3',
+  color: '#e74c3c',
+  fixedWidth: true,
+  tabs: [
+    { title: '选项1', disabled: false },
+    { title: '选项2', disabled: false },
+    { title: '选项3', disabled: false },
+    { title: '选项4', disabled: false },
+    { title: '选项5', disabled: false }
+  ]
 })
